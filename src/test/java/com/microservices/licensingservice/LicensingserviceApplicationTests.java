7cc5eb1ebba48ebf15549f258a661e03f88f3624
Hegde, Gajanan <Gajanan.Hegde@ellucian.com>
2024-08-16 19:42:54 +0530

    Initial Commit

---
+++ src/test/java/com/microservices/licensingservice/LicensingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.licensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
